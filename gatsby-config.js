42a80da5facb20f0e158860cb963ec4fe2b951b1
Maxime Ealet <maxime.ealet@ezeeworld.com>
2022-12-08 11:09:00 +0100

    error with graphql datocms query

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,34 @@
  */
 module.exports = {
   siteMetadata: {
-    title: ``,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Générateur de signature - Onefirst`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-datocms',
-    options: {
-      "apiToken": "6826e427293d27a6773e4d6b0f390d"
-    }
-  }]
+  plugins: [
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area. Make sure to grant both CDA and CMA permissions.
+        apiToken: `6826e427293d27a6773e4d6b0f390d`,
+
+        // The project environment to read from. Defaults to the primary environment:
+        environment: `main`,
+
+        // If you are working on development/staging environment, you might want to
+        // preview the latest version of records instead of the published one:
+        previewMode: false,
+
+        // Disable automatic reloading of content when some change occurs on DatoCMS:
+        disableLiveReload: false,
+
+        // Custom API base URL (you probably don't need this!)
+        // apiUrl: 'https://site-api.datocms.com',
+
+        // Limits page size and can be used to avoid build timeouts.
+        // Default is 500 (also the maximum)
+        pageSize: 500,
+      },
+    },
+  ],
 };
---
+++ src/components/seo.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { useSiteMetadata } from "../hooks/use-site-metadata"
+
+export const Seo = ({ title, pathname }) => {
+    const { title: defaultTitle, siteUrl } = useSiteMetadata()
+
+    const seo = {
+        title: title || defaultTitle,
+        url: `${siteUrl}${pathname || ``}`,
+    }
+
+    return (
+        <>
+            <title>{seo.title}</title>
+        </>
+    )
+}
